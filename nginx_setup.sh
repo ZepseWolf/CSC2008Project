f41a7fc51aeffede014b852732a7b140eaab4fdf
johnwang <monkeygoessnap@gmail.com>
2023-03-28 17:52:59 +0800

    changed nginx conf

--- nginx_setup.sh
+++ nginx_setup.sh
@@ -1,5 +1,5 @@
 cp edoc_conf /etc/nginx/sites-available
-ln -s /etc/nginx/sites-available/edoc_conf /etc/nginx/sites-enabled
+ln -s /etc/nginx/sites-available/digidex_conf /etc/nginx/sites-enabled
 unlink /etc/nginx/sites-enabled/default
 nginx -t
 systemctl restart nginx
